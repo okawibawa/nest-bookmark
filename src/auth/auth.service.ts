a0961cd262b88ebe7a72f8354880b0a3505cdf42
Oka Wibawa <okaa.wibawa@gmail.com>
2022-02-23 01:03:58 +0800

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save new user to db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      // return user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken!');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    // check if user exist
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // throw exception if user does not exist
+    if (!user) throw new ForbiddenException('Credentials incorrect!');
+
+    // check if password matches
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // throw exception if password does not match
+    if (!pwMatches) throw new ForbiddenException('Password incorrect!');
+
+    // return user
+    delete user.hash;
+
+    return user;
+  }
+}
